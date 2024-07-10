6cc9ca3d8a9c5b94adfa132bdb1bdddccd5a51c9
sanoop <sanooppoly777@gmail.com>
2024-07-10 14:46:30 +0530

    code cleanup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import 'react-native-reanimated'; // TODO: do we need this ?
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { MD3DarkTheme, MD3LightTheme, PaperProvider } from 'react-native-paper'; // FIXME: theming
 // Prevent the splash screen from auto-hiding before asset loading is complete.
